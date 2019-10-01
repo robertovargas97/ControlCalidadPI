6196ebb16ba3a14efc6294681151799d6ad3d235
Roberto Vargas Rojas <robertovargasrojas97@gmail.com>
2019-10-01 01:09:42 -0600

    Navbar mejorada

--- ControlCalidad/Scripts/materializeScripts.js
+++ ControlCalidad/Scripts/materializeScripts.js
@@ -1,19 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems);
-
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
 
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems);
-
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, options);
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, { coverTrigger: false, closeOnClick: true });
 
 });
 
