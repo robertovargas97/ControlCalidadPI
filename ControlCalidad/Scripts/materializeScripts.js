5b0b70d9bc0d84b22e71d5de5939efbe92beeb57
Roberto Vargas Rojas <robertovargasrojas97@gmail.com>
2019-10-01 12:56:35 -0600

    Me vole el modelo again

--- ControlCalidad/Scripts/materializeScripts.js
+++ ControlCalidad/Scripts/materializeScripts.js
@@ -6,5 +6,8 @@ document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, { coverTrigger: false, closeOnClick: true });
 
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems);
+
 });
 
