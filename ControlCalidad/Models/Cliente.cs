4941aca5e92cbf5b77d1cce9b3dde3548c6104a0
Roberto Vargas Rojas <robertovargasrojas97@gmail.com>
2019-10-29 16:30:56 -0600

    Modelo cliente

--- ControlCalidad/Models/Cliente.cs
+++ ControlCalidad/Models/Cliente.cs
@@ -11,7 +11,8 @@ namespace ControlCalidad.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Cliente
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
