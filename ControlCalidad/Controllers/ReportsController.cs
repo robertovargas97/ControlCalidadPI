5b841a81b1ff3195c84cf5d2f1450ef09cc2a564
Sebastián <alfaroleonsebas97@gmail.com>
2019-11-04 18:36:10 -0600

    flujo

--- ControlCalidad/Controllers/ReportsController.cs
+++ ControlCalidad/Controllers/ReportsController.cs
@@ -15,18 +15,22 @@ namespace ControlCalidad.Controllers
         //Controllers to get values from other models
         private ProjectController projectController = new ProjectController();
         private EmployeeController employeeController = new EmployeeController();
+        private QASystemEntities db = new QASystemEntities();
 
         // GET: Reportes
-        public ActionResult Index( string x)
+        public ActionResult Index(string proy)
         {
             ViewBag.allprojects = projectController.GetProjects();
             ViewBag.alltesters = employeeController.GetTesters();
-            return View();
+
+            string query = "EXEC PA_req_terminados_proy" + "'" + "Nuevo PA" + "'";
+            var tempEnded = (db.Database.SqlQuery<Reqterminados>(query)).ToList();
+
+            return View(tempEnded);
         }
 
-        public ActionResult CompletedRequirements(string x)
+        public ActionResult CompletedRequirements()
         {
-            
             return View();
         }
         public ActionResult RunningRequirements()
---
+++ ControlCalidad/Models/ReportsModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ControlCalidad.Models
+{
+    public class Reqterminados {
+        public string nombreProyecto { set; get; }
+        public string nombre { set; get; }
+        public string estado { set; get; }
+        public string complejidad { set; get; }
+        public string fechaFinalizacion { set; get; }
+        public int duracionReal { set; get; }
+        public string nombreP { set; get; }
+    }
+
+}
