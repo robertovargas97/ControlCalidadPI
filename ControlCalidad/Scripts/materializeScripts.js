a275506e7790cd2c3a52bc14a368b095df5599aa
Sebastián <alfaroleonsebas97@gmail.com>
2019-12-02 02:18:57 -0600

    Solamente quité la primer consulta: Total de Requerimientos

--- ControlCalidad/Scripts/materializeScripts.js
+++ ControlCalidad/Scripts/materializeScripts.js
@@ -191,7 +191,6 @@ function getTesterData() {
 
 //<summary> :  shows and hides the html components related with the report
 function showTesterReqHours() {
-    document.getElementById('barra1').style.display = "none";
     document.getElementById('barra2').style.display = "none";
     document.getElementById('barra3').style.display = "none";
     document.getElementById('barra4').style.display = "none";
@@ -201,9 +200,9 @@ function showTesterReqHours() {
     document.getElementById('barra8').style.display = "none";
     document.getElementById('barra9').style.display = "none";
     document.getElementById('barra10').style.display = "none";
+    document.getElementById('barra11').style.display = "none";
 
     document.getElementById('testerReqHoursBar').style.display = "block";
-    document.getElementById('consulta1').style.display = "none";
     document.getElementById('consulta2').style.display = "none";
     document.getElementById('consulta3').style.display = "none";
     document.getElementById('consulta4').style.display = "none";
@@ -214,6 +213,7 @@ function showTesterReqHours() {
     document.getElementById('testerReqHours').style.display = "block";
     document.getElementById('consulta9').style.display = "none";
     document.getElementById('consulta10').style.display = "none";
+    document.getElementById('consulta11').style.display = "none";
 
 }
 
